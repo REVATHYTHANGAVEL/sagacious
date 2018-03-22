d9fbda494f0c61b99dc123fe34fd1b6de944c9e9
REVATHYTHANGAVEL <37656399+REVATHYTHANGAVEL@users.noreply.github.com>
2018-03-22 15:51:18 +0530

    Create cosonant or not.py

---
+++ cosonant or not.py
@@ -0,0 +1,10 @@
+l = input("Input a letter of the alphabet: ")
+
+if l in ('a', 'e', 'i', 'o', 'u'):
+	print("%s It is a vowel." % l)
+elif l == 'y':
+	print("Sometimes letter y stand for vowel, sometimes stand for consonant.")
+else:
+	print("%s It is a consonant." % l) 
+	
+ 
